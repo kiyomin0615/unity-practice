4818c17b176bc2a60bcd2bf2d3309515463b2ba1
Kyomin Ku <moeday1010@gmail.com>
2023-11-29 21:22:02 +0900

    Getting the Manager component with Singleton pattern

---
+++ Assets/Scripts/Managers/Manager.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Manager : MonoBehaviour
+{
+    // Singleton Pattern
+    // static 멤버는 "유일"하고 "공유"된다
+    static Manager instance;
+    public static Manager Instance { get { Init(); return instance; }  }
+    void Start()
+    {
+        Init();
+    }
+
+    void Update()
+    {
+        
+    }
+
+    static void Init()
+    {
+        if (instance == null)
+        {
+            // 게임 오브젝트를 찾아서 리턴한다
+            GameObject managerGameObject = GameObject.Find("@Manager");
+
+            if (managerGameObject == null )
+            {
+                // 게임 오브젝트 생성한다
+                managerGameObject = new GameObject { name = "@Manager" };
+                // 게임 오브젝트에 컴포넌트를 추가한다
+                managerGameObject.AddComponent<Manager>();
+            }
+
+            // 씬을 불러올 때 게임 오브젝트를 제거하지 않는다
+            DontDestroyOnLoad(managerGameObject);
+            // 게임 오브젝트에서 컴포넌트를 가져와서 리턴한다
+            instance = managerGameObject.GetComponent<Manager>();
+        }
+
+    }
+}
